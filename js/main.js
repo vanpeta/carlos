8d3c79e4041328ffa855a1080d00577dba4d4eeb
Carlos Allende <carlosallendesanz@gmail.com>
2016-06-06 12:00:36 -0700

    fixing

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if($(window).scrollTop()>65){
             $("#navbar").css({transition: "background-color 1s", "background-color": "#ffffff"})
-            $("#name, #about, #portfolo, #contact").css({transition: "color 1s", "color": "#e64a19"})
+            $("#name, #about, #portfolio, #contact").css({transition: "color 1s", "color": "#e64a19"})
             $("#name").css({transition: "font-size 1s", "font-size": "25px"})
         } else {
           $("#navbar").css({transition: "background-color 1s", "background-color":"transparent"})
