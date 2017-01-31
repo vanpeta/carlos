10108713ec009d417d11944d12fb30988be43987
Carlos Allende <carlosallendesanz@gmail.com>
2017-01-30 22:13:20 -0800

    adding background

--- js/new-main.js
+++ js/new-main.js
@@ -2,13 +2,17 @@ $(document).ready(function(){
   $(window).scroll(function(){
     if($(window).scrollTop()>40){
       $('.box1').addClass('navBarOn')
-      setTimeout(function () {
-        $('nav').removeClass('hidden');
-      }, 1000);
+      $('nav').removeClass('hidden');
     } else {
-      $('.box1').removeClass('navBarOn')
-      $('nav').addClass('hidden')
+      $('.box1').removeClass('navBarOn');
+      $('nav').addClass('hidden');
     }
-  })
-})
+  });
+});
+var pattern = Trianglify({
+  width: window.innerWidth,
+  height: window.innerHeight
+});
+$('main').append(pattern.canvas());
+
 
